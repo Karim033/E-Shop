bd8ddb2e45c1863f3905f1dee1f223a51942d0e2
Karim033 <gaberk033@gmail.com>
2024-09-23 19:36:07 +0300

    Fix error in login endpoint

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -40,7 +40,8 @@ export const login = asyncHandler(async (req, res, next) => {
   const token = createToken(user._id);
   const cokkieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000
+      Date.now() +
+        Number(process.env.JWT_COOKIE_EXPIRE_TIME || 90) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
